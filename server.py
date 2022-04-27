0c0a43327fdda1b4b8f160e4da75d0fb06123e1a
thangSu <phamthang3003@gmail.com>
2022-04-27 21:01:42 +0700

    oke nha

--- server.py
+++ server.py
@@ -6,78 +6,113 @@ from threading import Thread
 import time
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S:")
-
-den1=1
-den2=1
-quat1=1
-quat2=1
+tb1=1
+tb2=1
+tb3=1
+tb4=1
+tem=0
+hum=0
 stop=0
-host=socket.gethostbyname(socket.gethostname())
-port=8080
+host="192.168.0.101"
+port=80
+phone=[]
+equipment=[]
 print(host,":",port)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
-s.listen(2)
+s.listen(3)
 print("listening.....")
+def xu_ly(js):
+    global tb2, tb1, tb3,tb4,tem,hum
+    if "tb1" in js.keys():
+
+        if js["tb1"]==0:
+            tb1=1
+            print("tb1 on")
+        else:
+            tb1=0
+            print("tb1 off")
+    if "tb2" in js.keys():
+
+        if js["tb2"]==0:
+            tb2=1
+            print("tb2 on")
+        else:
+            tb2=0
+            print("tb2 off")
+    if "tb3" in js.keys():
+
+        if js["tb3"]==0:
+            tb3=1
+            print("tb3 on")
+        else:
+            tb3=0
+            print("tb3 off")
+    if "tb4" in js.keys():
+
+        if js["tb4"]==0:
+            tb4=1
+            print("tb4 on")
+        else:
+            tb4=0
+            print("tb4 off")
+    if "nd" in js.keys():
+        tem =js["nd"]
+    if "da" in js.keys():
+        hum =js["da"]
 def send_phone(client):
-    while True:
-        tem = random.randint(0, 100)
-        hum = random.randint(0, 100)
-        data_send = '"nd":{tem},"da":{hum},"b1":{d1},"b2":{d2},"q1":{q1},"q2":{q2}'.format(tem=tem, hum=hum, d1=den1,d2=den2, q1=quat1, q2=quat2)
+        data_send = '"nd":{tem},"da":{hum},"b1":{d1},"b2":{d2},"q1":{q1},"q2":{q2}'.format(tem=tem, hum=hum, d1=tb1,d2=tb2, q1=tb3, q2=tb4)
+        dat_print= "SEND:" + data_send
         data_send = '{' + data_send + '}\n'
-        dat_print='SEND: "nd":{tem},"da":{hum},"b1":{d1},"b2":{d2},"q1":{q1},"q2":{q2}'.format(tem=tem, hum=hum, d1=den1,d2=den2, q1=quat1, q2=quat2)
         client.send(str.encode(data_send))
         print(dat_print)
-        time.sleep(1)
-
-def handle_threading(client,addr):
-    global den2, quat1, quat2
-    try:
-        while True:
-            data = client.recv(1024)
-            str_data = data.decode("utf8")
-            if str_data == "quit":
-                print(addr, "is disconnected")
-                print("Listening...")
-                break
+def equip_threading(client,addr):
+    while True:
+        data = client.recv(1024)
+        str_data = data.decode("utf8")
+        if str_data == "quit":
+            print(addr, "is disconnected")
+            break
+        data_out = "{time} [{add}:{port}]: {str}".format(time=current_time,add=addr[0],port=port,str=str_data)
+        print("equip: ",data_out)
+        js = json.loads(str_data)
+        xu_ly(js)
+        if len(phone) == 0:
+            print("không có thiết bị nào")
+        else:
+            send_phone(phone[0])
+def phone_threading(client,addr):
+    global tb2, tb1, tb3,tb4,tem,hum
+    while True:
+        data = client.recv(1024)
+        str_data = data.decode("utf8")
+        if str_data == "quit":
+            print(addr, "is disconnected")
+            del phone[0]
+            break
+        else:
             data_out = "{time} [{add}:{port}]: {str}".format(time=current_time,add=addr[0],port=port,str=str_data)
-            print(data_out)
-            js = json.loads(str_data)
+            print("phone: ",data_out)
+            if len(phone) == 0:
+                print("không có thiết bị nào")
+            else:
+                equipment[0].send(str.encode(str_data+"\r\n"))
 
-            if "den2" in js.keys():
-                den2=~den2
-                if den2==1:
-                    print("Đèn 2 đang bật")
-                else:
-                    print("Đèn 2 đang tắt")
-            if "quat1" in js.keys():
-                quat1=~quat1
-                if quat1==1:
-                    print("Quạt 1 đang bật")
-                else:
-                    print("Quạt 1 đang tắt")
-            if "quat2" in js.keys():
-                quat2=~quat2
-                if quat2==1:
-                    print("Quạt 2 đang bật")
-                else:
-                    print("Quạt 2 đang tắt")
-    except:
-        print(addr,"is disconnected")
-        print("Listening...")
-        a.join()
-        print(a.is_alive())
 def connect():
     while True:
         client, addr = s.accept()
         print('Connected by', addr[0])
-        global a,b
-        a=Thread(target=send_phone, args=(client,))
-        a.start()
-        print(a.is_alive())
-        b = Thread(target=handle_threading, args=(client,addr,))
-        b.start()
-        print(b.is_alive())
+        data = client.recv(1024)
+        str_data = data.decode("utf8")
+        print(str_data)
+        if str_data == "phone":
+            phone.append(client)
+            Thread(target=phone_threading, args=(client,addr,)).start()
+            print("điện thoại đã kết nối")
+        if str_data == "equip":
+            equipment.append(client)
+            Thread(target=equip_threading, args=(client,addr,)).start()
+            print("thiết bị đã kết nối")
 
 if __name__ == "__main__":
     s.listen(5)
